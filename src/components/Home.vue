cdcda4db72450a8648b7f314bd00ac5520c3340a
Ogura Katsuya <k-ogura@ist.osaka-u.ac.jp>
2018-03-09 08:33:11 +0900

    現在位置の取得成功&Google MAPの呼び出しができない

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,10 +4,12 @@
       </app-header>
         <!-- マップ -->
         <div id="google-map">
+           <!--
            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11278.640573196893!2d135.4963911622769!3d34.6721549189598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e71526dc6083%3A0xe43d6b861e80b709!2z44CSNTQyLTAwNzEg5aSn6Ziq5bqc5aSn6Ziq5biC5Lit5aSu5Yy66YGT6aCT5aCA!5e0!3m2!1sja!2sjp!4v1520502552924"
             frameborder="0"
             style="border:0">
            </iframe> 
+           -->
         </div>
         <div class="subheader">
             <p>この辺にあるツッコミポイント</p>
@@ -27,6 +29,7 @@
 import Vue from "vue";
 import TukkomiListItem from "./tukkomiListItem.vue";
 import AppHeader from "./AppHeader.vue";
+
 
 export default Vue.extend({
   name: "App",
@@ -65,6 +68,46 @@ export default Vue.extend({
     };
   }
 });
+// 現在位置の取得
+navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+
+	// 取得成功
+	function geoSuccess(position) {
+		// 緯度
+		var lat = position.coords.latitude;
+		// 経度
+    var lng = position.coords.longitude;
+    console.log(lat + ", " + lng);
+    /*
+		//地図の作成
+		var map = new google.maps.Map(document.getElementById('google-map'), {
+			zoom: 18, //地図の倍率
+			center: {lat: lat, lng: lng} //中心の座標
+		});
+		//マーカーのインスタンス
+		var marker = new google.maps.Marker({
+			position: {lat: lat, lng: lng}, //マーカーの位置
+			map: map //マーカーを描く地図
+    });
+    */
+	}
+	// 取得失敗(拒否)
+	function geoError() {
+		console.log('取得NG');
+  }
+
+  // Google MAP APIの利用
+  function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('google-map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
 </script>
 
 <style lang="scss" scoped>
